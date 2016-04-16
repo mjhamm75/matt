8c7ad87e3aaeb0aa35d41ee9ad3e4c26f73603e5
mjhamm75 <jason.hamm@ldschurch.org>
2016-04-16 16:34:20 -0600

    ajax updates

--- public/application.js
+++ public/application.js
@@ -2,9 +2,19 @@ $('#button').on('click', function(e) {
   e.preventDefault();
   var title = $('#title');
   var pi = $('#pi');
-  // $.ajax('https://internalgrantdev.azurewebsites.net/api/Grants/', {
-  //   method: 'POST'
-  // })
-  // .done()
-  // .catch()
+
+  $.ajax('https://internalgrantdev.azurewebsites.net/api/Grants/', {
+    method: 'POST'
+  })
+  .done(function (result) {
+    debugger;
+  })
+  .error(function(err) {
+    debugger;
+    // $('#error-message').innerHtml("Is Broken")
+  })
 })
+
+function clear(element) {
+  element[0].value = "";
+}
