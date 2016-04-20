b0febfe7fbddfad1df50f83d6d7c575a97f97db0
Matthew Day <matt@matthewday.net>
2016-04-20 09:56:59 -0400

    Testing to see if Express server is necessary

--- public/submission-detail.js
+++ public/submission-detail.js
@@ -66,7 +66,7 @@ function updateGrantView(grant) {
     $('#edit-view').append(inputDiv);
 
     $(document).on('click', '#update', function(e) {
-        $.ajax('/grants/'+ grantId, {
+        $.ajax('http://internalgrantdev.azurewebsites.net/api/MGrants/'+ grantId, {
                 method: 'PUT',
                 data: {
                     "Id": inputId.val(),
@@ -95,7 +95,9 @@ function updateGrantView(grant) {
     });
 }
 
-$.get('/grants/' + grantId)
+var GRANT_URL = 'http://internalgrantdev.azurewebsites.net/api/MGrants/';
+
+$.get(GRANT_URL + grantId)
     .done(function(res) {
         updateGrantView(res.grant);
     })
